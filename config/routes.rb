ef89c2b2a898a5b2117dfb1d41bc95aecbf976bb
AleMint <ale.helguero@gmail.com>
2010-08-16 08:31:39 -0400

    Añadido contactos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :contactos
+
   map.resources :consultas
 
   map.resources :importaciones, :member => { :cruce => :get }
---
+++ db/migrate/20100816112409_create_contactos.rb
@@ -0,0 +1,16 @@
+class CreateContactos < ActiveRecord::Migration
+  def self.up
+    create_table :contactos do |t|
+      t.string :nombre
+      t.string :cargo
+      t.string :telefono
+      t.string :email
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contactos
+  end
+end
