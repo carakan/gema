fba118829b71da4c7cceb642eb030d2ef54a4f83
Boris Barroso <boriscyber@gmail.com>
2010-08-19 18:21:18 -0400

    Usado CoffeeScript para javascript y mejorados algunos estilos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,271 +1,239 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-jQuery(function($) {
-
-  var speed = 300,
-     csfr_token = $('meta[name=csfr-token]').attr('content');
-
-  // Mostrar ocultar detalle
-  $('a.more').live("click", function() {
-    $(this).html('Ver menos').removeClass('more').addClass('less').next('.hidden').show(speed);
-  });
-  $('a.less').live('click', function() {
-    $(this).html('Ver mas').removeClass('less').addClass('more').next('.hidden').hide(speed);
-  });
-
-
-  /**
-   * Presenta errores en contexto
-   */
-  $('[alt]').live('mouseover mouseout', function(e) {
-    var div = '#tooltip';
-    if($(this).hasClass('error') ) {
-      div = '#tooltip-error';
-    }
-    if(e.type == 'mouseover') {
-      var pos = $(this).position();
-
-      $(div).css({
-        'top': (pos.top - 30) + 'px', 'left': (e.clientX + 20) + 'px'
-      }).html( $(this).attr('alt') );
-      $(div).show();
-    }else{
-      $(div).hide();
-    }
-  });
-
-  // Para presentar la imagen en tamaño original
-  $('img.mini').live('click', function() {
-    var lista = $(this).attr("src").split("/");
-    var l = lista.length - 1;
-    lista[l] = lista[l].replace(/^mini/, 'original');
-    var div = document.createElement('div');
-    $(div).html(['<img', ' src=', '"', lista.join("/") , '"', " />"].join("") );
-
-    $(div).dialog({
-        'width': 700, 'height': 500, 
-        'close': function(e, ui) { $(div).dialog('destroy');  }
-    });
-  });
-
-  /**
-   * Para presentar formulario AJAX
-   */
-  $('a.ajax').live("click", function(e) {
-    var id = (new Date).getTime().toString();
-    $(this).attr('data-ajax_id', id);
-    var div = document.createElement('div');
-    $(div).attr( 'id', (new Date).getTime.toString() ).addClass('ajax-modal').css( { 'z-index': 1000 } ).attr('data-ajax_id', id);
-    $(div).load( $(this).attr("href") );
-    $(div).dialog({ 'width': 800, 'height' : 400, 'modal': true, 'resizable' : false });
-
-    e.stopPropagation();
-    return false;
-    // Crear formulario  llenarlo
-  });
-
-  /**
-   * Hace submit de un formulario
-   */
-  $('div.ajax-modal form').live('submit', function() {
-
-    var data = serializeFormElements(this);
-    var el = this;
-
-    $.ajax({
-      'url': $(el).attr('action'),
-//      'cache': false,
-      'context':el,
-      'data':data,
-      'type': (data['_method'] || 'post'),
-      'success': function(resp) {
-      },
-      'complete': function(resp) {
-        var p = $(el).parents('div.ajax-modal');
-        var id = $(p).attr('data-ajax_id');
-        $(p).dialog('destroy');
-        $(p).remove();
-        $('body').trigger('ajax:completed', [id]);
-      },
-      'error': function(resp) {
-        alert('Existen errores en su formulario por favor corrija los errores');
+(function() {
+  var getTarget;
+  $(document).ready(function() {
+    var addDatePicker, csfr_token, iniciar, mark, parsearFecha, serializeFormElements, setFechaDateSelect, speed, transformarDateSelect;
+    speed = 300;
+    csfr_token = $('meta[name=csfr-token]').attr('content');
+    $.datepicker._defaults.dateFormat = 'dd M yy';
+    parsearFecha = function(fecha) {
+      fecha = $.datepicker.parseDate($.datepicker._defaults.dateFormat, fecha);
+      return [fecha.getFullYear(), fecha.getMonth() + 1, fecha.getDate()];
+    };
+    setFechaDateSelect = function(el) {
+      var fecha;
+      fecha = parsearFecha($(el).val());
+      $(el).siblings('select[name*=1i]').val(fecha[0]);
+      $(el).siblings('select[name*=2i]').val(fecha[1]);
+      return $(el).siblings('select[name*=3i]').val(fecha[2]);
+    };
+    transformarDateSelect = function() {
+      return $('li.date, div.date').each(function(i, el) {
+        var day, input, month, year;
+        input = document.createElement('input');
+        $(input).attr({
+          'class': 'date-transform',
+          'type': 'text'
+        });
+        year = $(el).find('select[name*=1i]').hide().val();
+        month = parseInt($(el).find('select[name*=2i]').hide().val()) - 1;
+        day = $(el).find('select[name*=3i]').hide().after(input).val();
+        $(input).datepicker({
+          'showOn': 'button',
+          'buttonImage': '/images/icons/calendar.gif',
+          'buttonImageOnly': true
+        }).change(function(e) {
+          return setFechaDateSelect(this);
+        });
+        return $(input).datepicker("setDate", new Date(year, month, day));
+      });
+    };
+    $('[alt]').live('mouseover mouseout', function(e) {
+      var div, pos;
+      div = '#tooltip';
+      if (($(this).hasClass('error'))) {
+        div = '#tooltip-error';
+      };
+      if ((e.type === 'mouseover')) {
+        pos = $(this).position();
+        $(div).css({
+          'top': (pos.top - 30) + 'px',
+          'left': (e.clientX + 20) + 'px'
+        }).html($(this).attr('alt'));
+        return $(div).show();
+      } else {
+        return $(div).hide();
       }
     });
-
-    return false;
-  });
-
-  /***************************/
-
-  // Date select, progressive enhancement
-  $.datepicker._defaults.dateFormat = 'dd M yy';
-
-  
-
-  $('input.date').live('change', function() {
-    var d = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $(this).val() );
-    $(this).prev('input').val( [ d.getFullYear(), (d.getMonth() + 1), d.getDate() ].join("-") );
-  });
-
-  /*****************************/
-
-  /**
-   * Llama a todas las funciones que deben realizarse cuando se carga el DOM normalmente o AJAX
-   */
-  function callFunctions() {
-    addDatePicker();
-  }
-
-  callFunctions();
-
-  /* AJAX configuration*/
-
-  $('body').ajaxComplete(function() {
-    callFunctions();
-  });
-
-
-  $('a.delete').live("click", function(e) {
-    $(this).parents("tr:first, li:first").addClass('marked');
-    if(confirm('Esta seguro de borrar el item seleccionado')) {
-      var url = $(this).attr('href');
-      var el = this;
-
-      $.ajax({
-        'url': url,
-        'type': 'delete',
-        'context': el,
-        'success': function() {
-          $(el).parents("tr:first, li:first").remove();
-
-          $('body').trigger('ajax:delete', url);
-        },
-        'error': function() {
-          alert('Existio un error al borrar');
+    $('a.more').live("click", function() {
+      return $(this).html('Ver menos').removeClass('more').addClass('less').next('.hidden').show(speed);
+    });
+    $('a.less').live('click', function() {
+      return $(this).html('Ver mas').removeClass('less').addClass('more').next('.hidden').hide(speed);
+    });
+    $('img.mini').live('click', function() {
+      var div, l, lista;
+      lista = $(this).attr("src").split("/");
+      l = lista.length - 1;
+      lista[l] = lista[l].replace(/^mini/, 'original');
+      div = document.createElement('div');
+      $(div).html(['<img', ' src=', '"', lista.join("/"), '"', " />"].join(""));
+      return $(div).dialog({
+        'width': 700,
+        'height': 500,
+        'close': function(e, ui) {
+          return $(div).dialog('destroy');
         }
       });
-
-    }else{
-      $(this).parents("tr:first, li:first").removeClass('marked');
+    });
+    $('a.ajax').live("click", function(e) {
+      var div, id;
+      id = (new Date()).getTime().toString();
+      $(this).attr('data-ajax_id', id);
+      div = document.createElement('div');
+      $(div).attr('id', (new Date()).getTime.toString()).addClass('ajax-modal').css({
+        'z-index': 1000
+      }).attr('data-ajax_id', id);
+      $(div).load($(this).attr("href"));
+      $(div).dialog({
+        'width': 800,
+        'height': 400,
+        'modal': true,
+        'resizable': false
+      });
       e.stopPropagation();
-    }
-
-    return false;
-  });
-
-  /****************/
-    /**
-     * Sets the date for each select with the date selected with datepicker
-     */
-    /*
-    $('input.ui-date-text').live('change', function() {
-        var sels = $(this).siblings("select:lt(3)");
-        var d = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $(this).val() );
-        
-        $(sels[0]).val(d.getFullYear());
-        $(sels[1]).val(d.getMonth() + 1);
-        $(sels[2]).val(d.getDate());
+      return false;
     });
-    */
-    
-    /**
-     * Replaces the date or datetime field with jquey-ui datepicker
-     */
-    /*
-    $('.date, .datetime').each(function(i, el) {
-        var input = document.createElement('input');
-        $(input).attr({'type': 'text', 'class': 'ui-date-text'});
-        // Insert the input:text before the first select
-        $(el).find("select:first").before(input);
-        $(el).find("select:lt(3)").hide();
-        // Set the input with the value of the selects
-        var values = [];
-        $(input).siblings("select:lt(3)").each(function(i, el) {
-            var val = $(el).val();
-            if(val != '')
-                values.push(val);
-        });
-        if( values.length > 1) {
-            d = new Date(values[2], parseInt(values[1]) - 1, values[0] );
-            $(input).val( $.datepicker.formatDate($.datepicker._defaults.dateFormat, d) );
+    $('div.ajax-modal form').live('submit', function() {
+      var data, el;
+      data = serializeFormElements(this);
+      el = this;
+      $.ajax({
+        'url': $(el).attr('action'),
+        'context': el,
+        'data': data,
+        'type': (data['_method'] || 'post'),
+        'success': function(resp) {},
+        'complete': function(resp) {
+          var id, p;
+          p = $(el).parents('div.ajax-modal');
+          id = $(p).attr('data-ajax_id');
+          $(p).dialog('destroy');
+          $(p).remove();
+          return $('body').trigger('ajax:completed', [id]);
+        },
+        'error': function(resp) {
+          return alert('Existen errores en su formulario por favor corrija los errores');
         }
-
-        $(input).datepicker({
-          'showOtherMonths': true, 
-          'selectOtherMonths': true, 
-          'buttonImage': '/images/icons/calendar.gif',
-          'showOn': 'button',
-    			'buttonImageOnly': true
-        }).attr('size', '14');
-    });
-    */
-});
-/*jQuery*/
-/*****************************************************************/
-
-/**
- * Serializa los datos de un formulario para hacer submit mediante AJAX
- */
-function serializeFormElements(elem) {
-  var params = {};
-
-  $(elem).find('input, select, textarea').each(function(i, el) {
-    params[ $(el).attr('name') ] = $(el).val();
-  });
-
-  return params;
-}
-
-
-/**
- * Adiciona un datePicker a todos los elementos con clase date
- */
-function addDatePicker() {
-  $('input.date').each(function(i, el) {
-    if(!$(el).hasClass('hasDate')) {
-
-      var input = document.createElement('input');
-
-      $(input).attr({'type': 'text', 'class': 'ui-date-text'});
-      $(el).addClass('hasDate hide').after(input);
- //     $(el).hide().attr({'type': 'hidden'});
-
-      var id = '#' + el.id;
-
-      $(input).datepicker({
-        'altFormat': 'yy-mm-dd',
-        'altField': id,
-        'showOtherMonths': true, 
-        'selectOtherMonths': true, 
-        'buttonImage': '/images/icons/calendar.gif',
-        'showOn': 'button',
-        'buttonImageOnly': true
       });
-      try{
-        var d = $.datepicker.parseDate('yy-mm-dd', $(el).val() );
-        d = $.datepicker.formatDate($.datepicker._defaults.dateFormat, d);
-        $(input).datepicker('setDate', d);
-      }catch(e) {}
-    }
+      return false;
+    });
+    addDatePicker = function() {
+      return $('input.date').each(function(i, el) {
+        var d, id, input;
+        if ((!$(el).hasClass('hasDate'))) {
+          input = document.createElement('input');
+          $(input).attr({
+            'type': 'text',
+            'class': 'ui-date-text'
+          });
+          $(el).addClass('hasDate hide').after(input);
+          id = '#' + el.id;
+          $(input).datepicker({
+            'altFormat': 'yy-mm-dd',
+            'altField': id,
+            'showOtherMonths': true,
+            'selectOtherMonths': true,
+            'buttonImage': '/images/icons/calendar.gif',
+            'showOn': 'button',
+            'buttonImageOnly': true
+          });
+          try {
+            d = $.datepicker.parseDate('yy-mm-dd', $(el).val());
+            d = $.datepicker.formatDate($.datepicker._defaults.dateFormat, d);
+            return $(input).datepicker('setDate', d);
+          } catch (e) {
+            return e;
+          }
+        }
+      });
+    };
+    $('input.date').live('change', function() {
+      var d;
+      d = parsearFecha($(this).val());
+      return $(this).prev('input').val([d[0], d[1], d[2]].join("-"));
+    });
+    $('a.delete').live("click", function(e) {
+      var el, url;
+      $(this).parents("tr:first, li:first").addClass('marked');
+      if ((confirm('Esta seguro de borrar el item seleccionado'))) {
+        url = $(this).attr('href');
+        el = this;
+        $.ajax({
+          'url': url,
+          'type': 'delete',
+          'context': el,
+          'success': function() {
+            $(el).parents("tr:first, li:first").remove();
+            return $('body').trigger('ajax:delete', url);
+          },
+          'error': function() {
+            return alert('Existio un error al borrar');
+          }
+        });
+      } else {
+        $(this).parents("tr:first, li:first").removeClass('marked');
+        e.stopPropagation();
+      }
+      return false;
+    });
+    serializeFormElements = function(elem) {
+      var params;
+      params = {};
+      $(elem).find('input, select, textarea').each(function(i, el) {
+        return (params[$(el).attr('name')] = $(el).val());
+      });
+      return params;
+    };
+    addDatePicker = function() {
+      return $('input.date').each(function(i, el) {
+        var d, id, input;
+        if ((!$(el).hasClass('hasDate'))) {
+          input = document.createElement('input');
+          $(input).attr({
+            'type': 'text',
+            'class': 'ui-date-text'
+          });
+          $(el).addClass('hasDate hide').after(input);
+          id = '#' + el.id;
+          $(input).datepicker({
+            'altFormat': 'yy-mm-dd',
+            'altField': id,
+            'showOtherMonths': true,
+            'selectOtherMonths': true,
+            'buttonImage': '/images/icons/calendar.gif',
+            'showOn': 'button',
+            'buttonImageOnly': true
+          });
+          try {
+            d = $.datepicker.parseDate('yy-mm-dd', $(el).val());
+            d = $.datepicker.formatDate($.datepicker._defaults.dateFormat, d);
+            return $(input).datepicker('setDate', d);
+          } catch (e) {
+            return e;
+          }
+        }
+      });
+    };
+    mark = function(selector, velocity, val) {
+      val = val || 0;
+      $(selector).css({
+        'background': 'rgb(255,255,' + val + ')'
+      });
+      if ((val >= 255)) {
+        return false;
+      }
+      return setTimeout(function() {
+        val += 5;
+        return mark(selector, velocity, val);
+      }, velocity);
+    };
+    iniciar = function() {
+      transformarDateSelect();
+      return addDatePicker();
+    };
+    return iniciar();
   });
-}
-
-/**
- * Mark
- * @param String // jQuery selector
- * @param Integer velocity
- */
-function mark(selector, velocity, val) {
-  val = val || 0;
-  $(selector).css({'background': 'rgb(255,255,'+val+')'});
-  if(val >= 255)
-    return false;
-  setTimeout(function() {
-    val+=5;
-    mark(selector, velocity, val);
-  }, velocity);
-}
-
-function getTarget(e) {
-  return e.target || e.srcElement;
-}
+  getTarget = function(e) {
+    return e.target || e.srcElement;
+  };
+})();
--- vendor/plugins/rorol/lib/rorol.rb
+++ vendor/plugins/rorol/lib/rorol.rb
@@ -1,9 +1,9 @@
-%w( models ).each do |dir|
-  path = File.join( File.dirname(__FILE__), '..' 'app', dir )
-  $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
-end
+#%w( models ).each do |dir|
+#  path = File.join( File.dirname(__FILE__), '..' 'app', dir )
+#  $LOAD_PATH << path
+#  ActiveSupport::Dependencies.load_paths << path
+#  ActiveSupport::Dependencies.load_once_paths.delete(path)
+#end
 
 # Necesario para que funcione correctamente los tasks
 module Rorol
