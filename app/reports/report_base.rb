9ad5c97ea95c726a177f8c60692201f94f0b8f7d
carakan <carakan@gmail.com>
2010-11-01 12:58:13 -0400

    Added correct position for images

--- app/reports/report_base.rb
+++ app/reports/report_base.rb
@@ -13,21 +13,29 @@ class ReportBase < Prawn::Document
     end
 
     fecha_publicacion = "#{I18n.l(@marca.fecha_publicacion, :format => :long) if @marca.fecha_publicacion}"
-    marca_header = [I18n.t("nombre marca"), I18n.t("clase marca"), I18n.t("numero solicitud marca"),
+    marca_header = [I18n.t("nombre marca"), "image", I18n.t("clase marca"), I18n.t("numero solicitud marca"),
                     I18n.t("fecha publicacion marca"), I18n.t("numero publicacion marca"),
                     I18n.t("titulares marca"), I18n.t("observaciones marca")]
-    marca_table = ["#{@marca.nombre}", "#{@marca.clase_id}", "#{@marca.numero_solicitud}", 
+    marca_table = ["#{@marca.nombre}", "", "#{@marca.clase_id}", "#{@marca.numero_solicitud}",
                    "#{fecha_publicacion}", "#{@marca.numero_publicacion}",
                    "#{@marca.titulares.collect{|representante| "#{representante.nombre}"}.join(", ")}", "#{@observacion}"]
 
     data = [marca_header, marca_table]
 
-    table(data, :cell_style => {:border_width => 1}) do
-      row(0).style :background_color => 'f0f0f0'
-      row(0).style :"size" => 9
-      row(1).style :"size" => 9
-      rows(1..7).width = 150
-      cells.style(:size => 10, :inline_format => true)
+    the_x = 0
+    the_y = 0
+
+    table(data, :cell_style => {:border_width => 1}) do |t|
+      t.row(0).style :background_color => 'f0f0f0'
+      t.row(0).style :"size" => 9
+      t.row(1).style :"size" => 9
+      t.rows(1..7).width = 100
+      t.cells.style(:size => 10, :inline_format => true)
+      the_x = t.cells[1,2].x
+      the_y = t.cells[1,2].y
+
+      t.cells[1,1].content = "\n"
+      image("#{Rails.root}/public/images/logo-orpan.jpg", :at => [the_x + 110, the_y - 20], :fit => [80, 80])
     end
   end
 
