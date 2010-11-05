71a20b9fcb1526b15b49407b49f74b1b8e4e5862
Boris Barroso <boriscyber@gmail.com>
2010-11-05 11:43:02 -0400

    Rol refactorizado para ser mas claro

--- vendor/plugins/rorol/app/models/rol.rb
+++ vendor/plugins/rorol/app/models/rol.rb
@@ -15,63 +15,39 @@ class Rol < ActiveRecord::Base
     name
   end
 
-  # Actualiza los valores de permisos basado en a lista de controladores
-  # en caso de que no exita algun valor no es tomado en cuenta
-  def actualizar
-    # Iterar a traves de la lista de controladores
-    # es necesario verificar esto por que pueden cambiar sin aviso y se debe comparar
-    # contra los valores que ya existen en permisos de un rol
+  # Updates the privileges based on the query from the database
+  def set_privileges
     lista = [] # Variable para almacenar temporalmente los nuevos permisos
     Rol.routes.each do |cont|
       # Asignacion a dos variables para no confundirse
-      controller, actions = cont.keys.first, cont.values.first
-      # Busqueda del permiso con el mismo controlador
-      permission = permissions.select{ |per| controller == per.controller }[0]
+      controller, actions = cont.first, cont.last
+      permission = permissions[permissions.index { |p| p.controller == controller }]
+
       if permission
-        tmp = {}
-        # Iterar acciones del controlador no del permiso
-        actions.each do |k,v|
-          tmp[k] = permission.actions[k] # Se asignara true si es que exite la accion en permisos y su valor sea true
-        end
-        # asignacion de las acciones al permiso, esto por que puede ser
-        # que se hayan cambiado las acciones del controlador y se tengan nuesvas acciones
-        permission.actions = tmp
+        # Delete the ones that don't exists
+        (permission.actions.keys - actions.keys).each { |v| permission.actions.delete(v) }
+        permission.actions = actions.merge( permission.actions )
       else
-        # Adición del nuevo controlador en caso de que no exista
         self.permissions_attributes = [ { :controller => controller, :actions => actions } ]
       end
     end
   end
 
   class << self
-    # Lista todos los controladores y acciones de la aplicacion
-    # Retorna un array con dos elementos de array donde el primer elemento
-    # es el controlador y el segundo es un hash con las acciones y valores por defecto "false"
-    # Ejemplo:
-    #   [["uno", {"index" => false}], ["dos", {"index" => false}]]
-    def list_controllers
-      controllers = Dir.glob("#{Rails.root}/app/controllers/*_controller.rb").map { |v| File.basename v }.sort
-      controllers.delete("application_controller.rb")
 
-      controllers.inject([]) do |arr, controller|
-        cont = controller.gsub(/\.rb$/, '').classify.constantize.new
-        actions = (cont.methods - cont.private_methods - cont.protected_methods - ApplicationController.new.methods).sort
-        arr << [controller.gsub(/_controller\.rb/, ''), actions.inject({}) { |h, v| h[v] = false; h} ]
-      end
+    # List the routes in an array like this
+    #   ["reportes", {"index"=>false, "create"=>false}]
+    def routes(default = false)
+      ActionDispatch::Routing::Routes.routes.map do |r| 
+        [ r.requirements[:controller], r.requirements[:action] ]
+      end.group_by(&:first).map { |v| [ v.first, Hash[v.last.map { |val| [val.last, default] }] ] }
     end
 
-    # Listado de rutas
-    def routes(allow = false)
-      ActionDispatch::Routing::Routes.routes.map { |r| [ r.requirements[:controller], r.requirements[:action] ]}.group_by do |v|
-        v.first
-      end.inject([]) { |arr, v| arr << { v.first => v.last.map(&:last).inject({}) { |h, val| h[val.to_sym] = allow; h } }; arr }
-    end
-
+    # Returns a hash of controllers
     def hash_controllers_actions(allow = false)
-      routes(allow).map { |c| { :controller => c.keys.first, :actions => c.values.first }  }
+      routes(allow).map { |v| { :controller => v.first, :actions => v.last } }
     end
 
   end
 
 end
-#{:controlador=>"adjuntos", :acciones=>{:create=>false, :destroy=>false, :edit=>false, :index=>false, :new=>false, :show=>false, :update=>false}}
