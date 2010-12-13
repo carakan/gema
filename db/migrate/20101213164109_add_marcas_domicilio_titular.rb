849704b20402dee1eff23949ffdab747f4e0996f
Boris Barroso <boriscyber@gmail.com>
2010-12-13 13:06:47 -0400

    posts y marcas

---
+++ db/migrate/20101213164109_add_marcas_domicilio_titular.rb
@@ -0,0 +1,9 @@
+class AddMarcasDomicilioTitular < ActiveRecord::Migration
+  def self.up
+    add_column :marcas, :domicilio_titular, :string
+  end
+
+  def self.down
+    remove_column :marcas, :domicilio_titular
+  end
+end
---
+++ db/migrate/20101213170041_add_posts_categoria.rb
@@ -0,0 +1,9 @@
+class AddPostsCategoria < ActiveRecord::Migration
+  def self.up
+    add_column :posts, :categoria, :string
+  end
+
+  def self.down
+    remove_column :posts, :categoria
+  end
+end
