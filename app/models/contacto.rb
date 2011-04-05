432504caccbbdc2213d0e79d8db9f9422da3615f
Huascar <h.mamani85@gmail.com>
2011-04-05 12:34:59 -0400

    Modify items, areas, proyectos

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -3,7 +3,9 @@
 # email: boriscyber@gmail.com
 class Contacto < ActiveRecord::Base
   belongs_to :representante
+  #has_many :proyectos
   validates_presence_of :nombre, :cargo, :telefono
   validates :email, :presence => true, :email => true
   #validates_format_of :email, :with => Constants::EMAIL_REG, :unless => lambda { |c| c.email.blank? }
 end
+
--- app/models/proyecto/proyecto.rb
+++ app/models/proyecto/proyecto.rb
@@ -1,9 +1,9 @@
 class Proyecto::Proyecto < ActiveRecord::Base
-  attr_accessible :cliente_id, :area_id, :titulo, :referencia_cliente, :prioridad
+  attr_accessible :representante_id, :area_id, :titulo, :referencia_cliente, :prioridad, :contacto_id
   set_table_name "proyectos"
   belongs_to :area
   belongs_to :representante
-
+  #belongs_to :contacto
   has_many :correspondencias
   has_many :instruccions
   has_many :proyecto_items
