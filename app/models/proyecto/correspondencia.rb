452bd7697587aab8e9f7e6a5b7f65c425df315f7
carakan <carakan@gmail.com>
2011-04-12 11:04:22 -0400

    Fixed js in coccon

--- app/models/proyecto/correspondencia.rb
+++ app/models/proyecto/correspondencia.rb
@@ -1,4 +1,5 @@
 class Proyecto::Correspondencia < ActiveRecord::Base
   belongs_to :proyecto
   #validates :contenido, :presence => true
+    set_table_name "correspondencias"
 end
--- public/javascripts/cocoon.js
+++ public/javascripts/cocoon.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-
   $('.add_fields').live('click', function() {
     var assoc   = $(this).attr('data-association');
-    var content = $(this).siblings('#' + assoc + '_fields_template').html();
+    var content = $(this).attr('data-template');
     var regexp_braced = new RegExp('\\[new_' + assoc + '\\]', 'g');
     var new_id  = new Date().getTime();
     var new_content = content.replace(regexp_braced, '[' + new_id + ']');
@@ -13,16 +12,13 @@ $(document).ready(function() {
     $(this).parent().before(new_content);
     return false;
   });
-
   $('.remove_fields.dynamic').live('click', function() {
     $(this).closest(".nested-fields").remove();
     return false;
   });
-
   $('.remove_fields.existing').live('click', function() {
     $(this).prev("input[type=hidden]").val("1");
     $(this).closest(".nested-fields").hide();
     return false;
   });
-
 });
