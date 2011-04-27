a8e52e46831ae6ddba5e4316915658cbfb8b0590
carakan <carakan@gmail.com>
2011-04-27 13:54:04 -0400

    Updated some functions

--- app/models/proyecto/instruccion.rb
+++ app/models/proyecto/instruccion.rb
@@ -4,6 +4,8 @@ class Proyecto::Instruccion < ActiveRecord::Base
   belongs_to :proyecto
   has_many :instruccion_detalles
   accepts_nested_attributes_for :instruccion_detalles
+
+  belongs_to :email, :foreign_key => :referencia_email, :class_name => "Proyecto::Correspondencia"
   
   has_many :tareas_revision, :conditions => {:estado_tarea => 'revision'}, :order => 'fecha_limite ASC', :class_name => 'Proyecto::InstruccionDetalle'
   has_many :tareas_pendientes, :conditions => {:estado_tarea => 'pendiente'}, :order => 'fecha_limite ASC', :class_name => 'Proyecto::InstruccionDetalle'
