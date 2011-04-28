b742c5e09d225f5c709345faed02f91c6a789935
carakan <carakan@gmail.com>
2011-04-28 11:22:48 -0400

    Fixed error on massive asignation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -361,11 +361,10 @@ function insert_fields(link, method, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + method, "g")
   var regexp2 = new RegExp("parent_id_regex", "g");
+  console.debug($(link).attr("data-replace"));
   if($(link).attr("data-replace")){
     content = content.replace(regexp2, $(link).attr("data-replace"))
     $(link).removeAttr("data-replace");
-  } else {
-    content = content.replace(regexp2, "")
   }
   if($(link).attr("data-parent")){
     $($(link).attr("data-parent")).before(content.replace(regexp, new_id));
