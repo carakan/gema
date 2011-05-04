477459367dedad15ee7ccb2ee6019b44b6814a67
carakan <carakan@gmail.com>
2011-05-04 14:20:26 -0400

    added counters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110504144400) do
+ActiveRecord::Schema.define(:version => 20110504165447) do
 
   create_table "adjuntos", :force => true do |t|
     t.string   "nombre"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(:version => 20110504144400) do
     t.datetime "updated_at"
     t.string   "asunto"
     t.integer  "temporal_id", :limit => 8
+    t.integer  "contador",                 :default => 0
   end
 
   create_table "importaciones", :force => true do |t|
