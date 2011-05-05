e62fd6570142cce542267a46f79615b8a2dca2e9
Huascar Mamani <h.mamani85@gmail.com>
2011-05-05 16:55:57 -0400

    Partial Adjuntos search variable memory this generate when init a new _form in proyectos

--- app/models/proyecto/correspondencia.rb
+++ app/models/proyecto/correspondencia.rb
@@ -4,6 +4,7 @@ class Proyecto::Correspondencia < ActiveRecord::Base
   set_table_name "correspondencias"
   
   has_many :adjuntos, :as => :adjuntable, :dependent => :destroy
+  accepts_nested_attributes_for :adjuntos, :allow_destroy => true
   
   def siguiente_id
     codigo = Proyecto::Correspondencia.select("max(contador) as conteo").where(["proyecto_id = ?", proyecto_id]).first
