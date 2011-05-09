e79f72266cc9f87c285f6e4be8657db86869be48
Alejandra Helguero <ale.helguero@gmail.com>
2011-05-09 18:54:58 -0400

    Modificado el show de correspondencias en show proyectos

--- app/helpers/proyecto/instruccion_detalles_helper.rb
+++ app/helpers/proyecto/instruccion_detalles_helper.rb
@@ -4,4 +4,10 @@ module Proyecto::InstruccionDetallesHelper
       render(instruccion_detalles) + content_tag(:div, nested_instruccion_detalles(sub_instruccion_detalles), :class => "nested_instruccion_detalles")
     end.join.html_safe
   end
+
+  def links_tareas(klass, estado_tarea)
+    case (estado_tarea)
+      when "pendiente" then link_to "Entregar", entrega_proyecto_proyecto_instruccion_instruccion_detall_path(@proyecto, instruccion_detall.instruccion, instruccion_detall), :class => 'terminada'
+    end
+  end
 end
