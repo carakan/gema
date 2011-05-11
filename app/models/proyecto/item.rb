2c7dc48c1408c6daf98e04b0d81a472606d9d9bf
Alejandra Helguero <ale.helguero@gmail.com>
2011-05-11 11:01:08 -0400

    Añadido relacion intermedia en modelo item

--- app/models/proyecto/item.rb
+++ app/models/proyecto/item.rb
@@ -2,7 +2,8 @@ class Proyecto::Item < ActiveRecord::Base
   #attr_accessible :sigla, :nombre, :codigo_categoria
   #attr_accessible :categoria_id, :sigla, :nombre
   set_table_name "items"
-  has_many :proyecto_items
+  has_many :items_relaciones
+  has_many :proyecto_items, :trougth => :items_relaciones, :source => :proyecto_item
   has_ancestry
   
   def to_s
